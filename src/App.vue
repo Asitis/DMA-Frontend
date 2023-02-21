53b98194b6a51b69c3c25ea01a666694cb43bd68
Alex Timmer <alex@solvermedia.nl>
2023-02-21 22:40:29 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { ref, onMounted } from 'vue';
-// import AlbumCard from './components/AlbumCard.vue'
-
-// export default {
-//   components: { AlbumCard },
-//   setup() {
-//     const alba = ref([]);
-//     const isLoading = ref(false);
-//     const albumGenreNames = async (genres) => {
-//       const genreNames = [];
-//       for (const genre of genres) {
-//         const response = await fetch(
-//           `https://www.demaandagavond.nl/wp-json/wp/v2/genre/${genre}`
-//         );
-//         const genreData = await response.json();
-//         genreNames.push(genreData.name);
-//       }
-//       return genreNames.length ? genreNames.join(", ") : "Unknown";
-//     };
-
-//     onMounted(async () => {
-//       isLoading.value = true;
-
-//       const response = await fetch(
-//         "https://www.demaandagavond.nl/wp-json/wp/v2/dma_alba"
-//       );
-
-//       const data = await response.json();
-
-//       alba.value = data;
-//       isLoading.value = false;
-//     });
-
-//     return {
-//       alba,
-//       isLoading,
-//       albumGenreNames,
-//     };
-//   },
-// };
 </script>
 
 <template>
