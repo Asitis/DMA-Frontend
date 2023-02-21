ccdd4552aee21abe9ba81e708731163bafb6e8ef
Alex Timmer <alex@solvermedia.nl>
2023-02-21 08:08:46 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <p v-if="isLoading">Loading...</p>
+    <ul v-for="album in alba" :key="album.id">
+      <h2>{{ album.title.rendered }}</h2>
+      <p>Notes: {{ album.acf.notes }}</p>
+      <p>Artist: {{ album.artist }}</p>
+      <p>Genres {{ album.genre }}</p> 
+      <p>Genre Name: {{ albumGenreName(album) }}</p>      
+      <p>Jaren: {{ album.jaren }}</p>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      alba: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async getGenreName(genres, genreId) {
+      const genre = genres.find((genre) => genre.id === genreId);
+      if (genre) {
+        const response = await fetch(
+          `https://www.demaandagavond.nl/wp-json/wp/v2/genre/${genre}`
+        );
+        const genreData = await response.json();
+        return genreData.name;
+      } else {
+        return null;
+      }
+    },
+  },
+  computed: {
+    albumGenreName() {
+      return async (album) => {
+        const genreName = await this.getGenreName(album.genre, 472);
+        return genreName || 'Unknown';
+      };
+    },
+  },
+  mounted() {
+    this.isLoading = true;
+    fetch("https://www.demaandagavond.nl/wp-json/wp/v2/dma_alba")
+      .then((response) => response.json())
+      .then((data) => {
+        this.alba = data;
+        this.isLoading = false;
+      });
+  },
+};
 </script>
 
 <style>
