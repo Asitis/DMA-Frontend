e37a1959d6115d6ff2a38c7ea2e3c53d567f881b
Alex Timmer <alex@solvermedia.nl>
2023-05-05 16:24:13 +0200

    updating artist pages

--- src/services/AlbaService.js
+++ src/services/AlbaService.js
@@ -82,77 +82,86 @@ export default {
         })
     },
 
-    getAlbumsByArtist(artistName) {
-        return apiClient.get('/artist', {
+  getAlbumsByArtist(artistName) {
+    return apiClient.get('/artist', {
+      params: {
+        search: artistName,
+        per_page: 1,
+      },
+    }).then((response) => {
+      const artist = response.data[0];
+      if (!artist) {
+        throw new Error(`No artist found with name "${artistName}"`);
+      }
+
+      return apiClient
+        .get('/dma_alba', {
           params: {
-            search: artistName,
-            per_page: 1,
+            artist: artist.id,
           },
-        }).then((response) => {
-          const artistId = response.data[0]?.id;
-          if (!artistId) {
-            throw new Error(`No artist found with name "${artistName}"`);
-          }
-          return apiClient
-            .get('/dma_alba', {
-              params: {
-                artist: artistId,
-              },
-            })
-            .then((response) => {
-              const albumPromises = response.data.map(album => {
-                // Get Genres
-                const genreIds = album.genre
-                const genrePromises = genreIds.map(id => {
-                  return apiClient.get(`/genre/${id}`).then(response => {
-                    const genreName = response.data.name;
+        })
+        .then((response) => {
+          const albumPromises = response.data.map(album => {
+            // Get Genres
+            const genreIds = album.genre
+            const genrePromises = genreIds.map(id => {
+              return apiClient.get(`/genre/${id}`).then(response => {
+                const genreName = response.data.name;
+                const div = document.createElement('div');
+                div.innerHTML = genreName;
+                return div.textContent;
+              });
+            });
+
+            // Get Artists
+            const artistId = album.artist
+            const artistPromise = Array.isArray(artistId)
+                ? Promise.all(artistId.map(id => apiClient.get(`/artist/${id}`).then(response => {
+                    const artistName = response.data.name;
                     const div = document.createElement('div');
-                    div.innerHTML = genreName;
+                    div.innerHTML = artistName;
                     return div.textContent;
-                  });
+                })))
+                .then(artistNames => artistNames.join(', '))
+                : apiClient.get(`/artist/${artistId}`).then(response => {
+                    const artistName = response.data.name;
+                    const div = document.createElement('div');
+                    div.innerHTML = artistName;
+                    return div.textContent;
                 });
 
-                // Get Artists
-                const artistId = album.artist
-                const artistPromise = Array.isArray(artistId)
-                    ? Promise.all(artistId.map(id => apiClient.get(`/artist/${id}`).then(response => {
-                        const artistName = response.data.name;
-                        const div = document.createElement('div');
-                        div.innerHTML = artistName;
-                        return div.textContent;
-                    })))
-                    .then(artistNames => artistNames.join(', '))
-                    : apiClient.get(`/artist/${artistId}`).then(response => {
-                        const artistName = response.data.name;
-                        const div = document.createElement('div');
-                        div.innerHTML = artistName;
-                        return div.textContent;
-                    });
-    
-                // Get Years
-                const yearId = album.jaren
-                const yearPromise = Array.isArray(yearId)
-                    ? Promise.all(yearId.map(id => apiClient.get(`/jaren/${id}`).then(response => response.data.name)))
-                    .then(yearNames => yearNames.join(', '))
-                    : apiClient.get(`/jaren/${yearId}`).then(response => response.data.name)
+            // Get Years
+            const yearId = album.jaren
+            const yearPromise = Array.isArray(yearId)
+                ? Promise.all(yearId.map(id => apiClient.get(`/jaren/${id}`).then(response => response.data.name)))
+                .then(yearNames => yearNames.join(', '))
+                : apiClient.get(`/jaren/${yearId}`).then(response => response.data.name)
 
-                // Get Images
-                const featuredImageId = album.featured_media
-                const featuredImagePromise = featuredImageId
-                  ? apiClient.get(`/media/${featuredImageId}`).then(response => {
-                    return response.data.source_url
-                  })
-                  : Promise.resolve(null)
-                return Promise.all([Promise.all(genrePromises), artistPromise, yearPromise, featuredImagePromise]).then(([genres, artist, jaren, featuredImageUrl]) => {
-                    album.genres = genres
-                    album.jaren = jaren
-                    album.artist = artist
-                    album.featuredImageUrl = featuredImageUrl
-                    return album
-                })
+            // Get Images
+            const featuredImageId = album.featured_media
+            const featuredImagePromise = featuredImageId
+              ? apiClient.get(`/media/${featuredImageId}`).then(response => {
+                return response.data.source_url
+              })
+              : Promise.resolve(null)
+            return Promise.all([Promise.all(genrePromises), artistPromise, yearPromise, featuredImagePromise]).then(([genres, artist, jaren, featuredImageUrl]) => {
+                album.genres = genres
+                album.jaren = jaren
+                album.artist = artist
+                album.featuredImageUrl = featuredImageUrl
+                return album
             })
-            return Promise.all(albumPromises)
-            });
+          })
+          return Promise.all(albumPromises).then(alba => {
+            return {
+              artist: {
+                name: artist.name,
+                description: artist.description,
+              },
+              alba: alba,
+            }
+          });
         });
-    }
+    });
+  }
 }
--- src/views/ArtistView.vue
+++ src/views/ArtistView.vue
@@ -2,6 +2,7 @@
   <div class="alba" ref="albumList">
         <div class="intro-card artist">
       <h1>{{ artist }}</h1>
+            <p>{{ description }}</p>
     </div>
 
     <AlbumCard v-for="album in alba" :key="album.id" :album="album" />
@@ -19,13 +20,16 @@ export default {
   data() {
     return {
       artist: null,
+      description: null,
       alba: []
     }
   },
   async created() {
     const { name } = this.$route.params;
-    this.artist = name;
-    this.alba = await AlbaService.getAlbumsByArtist(name);
+    const data = await AlbaService.getAlbumsByArtist(name);
+    this.artist = data.artist.name;
+    this.description = data.artist.description;  // This line is to get the description of the artist.
+    this.alba = data.alba;
   }
 }
 </script>
